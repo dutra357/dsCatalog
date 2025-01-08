6bc50d316f3e2c38b86ec9e8b0771cab4f565891
David Alves Dutra <dadutra@hotmail.com>
2025-01-08 12:11:28 -0300

    ProductController IT test.

--- src/test/java/com/dutra/dsCatalog/integration/ProductControllerIT.java
+++ src/test/java/com/dutra/dsCatalog/integration/ProductControllerIT.java
@@ -1,4 +1,46 @@
 package com.dutra.dsCatalog.integration;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Long existingId;
+    private Long notExistingId;
+    private Long totalProducts;
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        notExistingId = 999L;
+        totalProducts = 25L;
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+        ResultActions resultActions = mockMvc
+                .perform(MockMvcRequestBuilders.get("/products?page=0&size=12&sort=name,asc")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$..totalElements").value(25));
+    }
 }
